e57457c1c264e35819756bb09bc3a8ee217605d5
Link4Electronics <link4electronics@gmail.com>
2026-01-23 06:54:12 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,7 +14,7 @@ export DEPLOY_OPENGL=1
 export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/cro-mag-rally
+quick-sharun /usr/bin/CroMagRally
 
 # Additional changes can be done in between here
 
